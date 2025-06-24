b4822e238c8cc21467f7ac7a764d74b142a59fc4
BrayOm21 <cbi2233049595@izt.uam.mx>
2025-06-24 01:41:25 -0600

    Nuevo porgrama

--- Palabras.java
+++ Palabras.java
@@ -1,25 +1,30 @@
-public class Cadena 
+public class Palabras 
 {
     public static void main(String[] args) 
     {
         String texto = "Hola mundo";
         int total = regresaEntero(texto);
         System.out.println(total);
-
     }
 
     public static int regresaEntero(String texto)
     {
         if (texto == null || texto.isEmpty()) 
         {
-                    return 0;
+            return 0;
         }
         char[] c = texto.toCharArray();
         int total = 0;
         for (int i = 0; i < c.length; i++) 
         {
-            
+            if (Character.isLetter(c[i])) 
+            {
+                if (i == 0 || !Character.isLetter(c[i - 1])) 
+                {
+                    total++;
+                }
+            }
         }
         return total;
-    }
+    } 
 }
