3272e57d57fb17d5a07eda8e230c5f66d5b068ab
BrayOm21 <cbi2233049595@izt.uam.mx>
2025-06-23 19:54:56 -0600

    Desde PC2

---
+++ .vscode/Cadena.java
@@ -0,0 +1,28 @@
+public class Cadena 
+{
+    public static void main(String[] args) 
+    {
+        String texto = "Hola mundo";
+        int total = regresaEntero(texto);
+        System.out.println(total);
+
+    }
+
+    public static int regresaEntero(String texto)
+    {
+        if (texto == null || texto.isEmpty()) 
+        {
+                    return 0;
+        }
+        char[] c = texto.toCharArray();
+        int total = 0;
+        for (int i = 0; i < c.length; i++) 
+        {
+            if (Character.isDigit(c[i])) 
+            {
+                total++;
+            }
+        }
+        return total;
+    }
+}
