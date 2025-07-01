de722573272d1b4ddb5773b6f420fb5df1533793
BrayOm21 <cbi2233049595@izt.uam.mx>
2025-06-30 20:17:48 -0600

    Nuevos

--- ProgramaP5.java
+++ ProgramaP5.java
@@ -2,19 +2,29 @@ public class ProgramaP5
 {
     public static void main(String[] args) 
     {
-        String texto;
-        texto = "las vacas vuelan pero sólo a altura bajas";
-        int total = contarPalabras(texto);
+        String texto = "Un dia vi una vaca vestida de uniforme";
+        int total = regresaEntero(texto);
         System.out.println(total);
     }
 
-    public static int contarPalabras(String texto) 
+    public static int regresaEntero(String texto)
     {
         if (texto == null || texto.isEmpty()) 
         {
             return 0;
         }
-        String[] palabras = texto.split("\\s+");
-        return palabras.length;
-    }
+        char[] c = texto.toCharArray();
+        int total = 0;
+        for (int i = 0; i < c.length; i++) 
+        {
+            if (Character.isLetter(c[i])) 
+            {
+                if (i == 0 || !Character.isLetter(c[i - 1])) 
+                {
+                    total++;
+                }
+            }
+        }
+        return total;
+    } 
 }
