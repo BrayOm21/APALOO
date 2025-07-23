92a2c73dcfa8afd4097b8d6ab6602913edfaf170
BrayOm21 <cbi2233049595@izt.uam.mx>
2025-07-22 18:03:17 -0600

    Archivos clase

---
+++ Archivos.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.io.FileNotFoundException;
+
+public class Archivos 
+{
+    public static void main(String[] args) throws FileNotFoundException, IOException
+    {
+        leerTexto("C:\\Users\\braya\\Documents\\Git\\APALOO\\Java\\pg1513.txt");   
+    }    
+    public static void leerTexto(String arch) throws FileNotFoundException, IOException
+    {
+        RandomAccessFile f = new RandomAccessFile(arch, "r");
+        String l;
+        while((l=f.readLine()) != null)
+        {
+            System.out.println(l);
+        }
+        f.close();
+    }
+}
