e0936b146719692b27fff616c63990ba6feb457b
BrayOm21 <cbi2233049595@izt.uam.mx>
2025-07-29 16:15:32 -0600

    New

--- PracAPALOO/Recursividad.java
+++ PracAPALOO/Recursividad.java
@@ -4,8 +4,11 @@ public class Recursividad
     {    
         int i=5;
         int [] arr = {1,2,3,4};
+        int [] datos = {105,5,20,2,3,10,80,25,15,100};
         System.out.println("El factorial de tu numero es: "+recfactorial(i));
-        System.out.println("La suma de tu arreglo es: "+sumaArreglos(arr, 0));
+        System.out.println("La suma de tu arreglo 1 es: "+sumaArreglos(arr, 0));
+        System.out.println("El dato mas grand del arreglo 2 es : "+mayor(datos));
+
     }
     public static int recfactorial (int i)
     {
@@ -15,12 +18,26 @@ public class Recursividad
         }
         return i*recfactorial(i-1);
     } 
-    private static int sumaArreglos(int [] datos, int i)
+    public static int sumaArreglos(int [] datos, int i)
     {
         if(i==datos.length-1)
         {
             return datos[i];
         }
         return datos[i]+sumaArreglos(datos, i+1);
+    }
+    private static int mayor(int [] datos, int i, int j)
+    {
+        if(i==j)
+        {
+            return datos[i];
+        }
+        int mayorizq = mayor (datos, i, (i+j)/2);
+        int mayorder = mayor (datos, (i+j)/2+1, j);
+        return mayorizq>mayorder?mayorizq:mayorder;
+    }
+    public static int mayor(int [] datos)
+    {
+        return mayor(datos,0,datos.length-1);
     }
 }
